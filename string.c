ec76f1fe76d8eb52d61b9cf74f9761f0d7814547
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-05 16:11:48 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+int i;
+printf("List bef sorting\n");
+ for(i = 0; i <= max; i++)
+printf("%d ", a[i]);
+sort(0, max);
+printf("\nList afe sorting\n");
+for(i = 0; i <= max; i++)
+printf("%d ", a[i]);
+}
