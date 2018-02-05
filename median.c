c4f7168a18b2f19321ce328c103788fde4e3d704
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-05 16:02:30 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{int ar1[] = {1, 12, 15, 26, 38};
+int ar2[] = {2, 13, 17, 30, 45};
+int n1 = sizeof(ar1)/sizeof(ar1[0]);
+int n2 = sizeof(ar2)/sizeof(ar2[0]);
+if (n1 == n2)
+printf("Median is %d", getMedian(ar1, ar2, n1));
+else
+printf("Doesn't work for arrays of unequal size");
+getch();
+}
